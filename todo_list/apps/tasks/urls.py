d85de1b400f98fe24a7ea044adc2b077054a07e9
Ignacio JM Benedetti <tranceway@gmail.com>
2023-03-23 23:54:14 -0300

    Todo andando con intercooler

--- todo_list/apps/tasks/urls.py
+++ todo_list/apps/tasks/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import task_list, task_edit
+from .views import task_list, task_edit, task_new, task_delete, task_done, task_undone  # noqa
 
 urlpatterns = [
-    path('', task_list),
+    path('', task_list, name='task_list'),
+    path('new', task_new, name='task_new'),
     path('<int:task_id>/edit', task_edit, name='task_edit'),
-    path('<int:task_id>/done', task_edit, name='task_mark_done'),
+    path('<int:task_id>/done', task_done, name='task_done'),
+    path('<int:task_id>/undone', task_undone, name='task_undone'),
+    path('<int:task_id>/delete', task_delete, name='task_delete'),
 ]
--- todo_list/apps/tasks/views.py
+++ todo_list/apps/tasks/views.py
@@ -1,18 +1,34 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from .models import Task
 
 
 def task_list(request):
+    template = 'tasks/list.html'
+
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        template = 'tasks/list_partial.html'
 
     return render(
         request,
-        'tasks/list.html',
+        template,
         {
             'tasks_todo': Task.objects.filter(is_done=False),
             'tasks_done': Task.objects.filter(is_done=True),
         }
     )
+
+
+def task_new(request):
+    if request.method == 'POST':
+        task = Task()
+        task.user = request.user
+        task.description = request.POST['description']
+        task.save()
+
+    return redirect(reverse('task_list'))
 
 
 def task_edit(request, task_id):
@@ -26,3 +42,26 @@ def task_edit(request, task_id):
         task.save()
 
         return render(request, 'tasks/task_inline.html', {'task': task})
+
+
+def task_delete(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    task.delete()
+
+    return redirect(reverse('task_list'))
+
+
+def task_done(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    task.is_done = True
+    task.save()
+
+    return redirect(reverse('task_list'))
+
+
+def task_undone(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    task.is_done = False
+    task.save()
+
+    return redirect(reverse('task_list'))
